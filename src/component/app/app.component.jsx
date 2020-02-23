7caf107523c45ef59a29f2df004a672eab9c158f
biancalina <bianca.alyna30@gmail.com>
2020-02-23 15:44:04 +0000

    start working on my first calendar app

---
+++ src/component/app/app.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CalendarApp = () => {
+  return <div>Hello</div>
+}
+
+export { CalendarApp }
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,17 @@
-console.log('This is the bare bone setup for Remindly with React')
+import React from 'react'
+import { render } from 'react-dom'
+import { CalendarApp } from './component/app'
+
+const renderToDom = () => {
+  const calendarApp = document.querySelector('#calendar-app')
+  if(calendarApp !== null){
+    render(
+      <CalendarApp/>,
+      calendarApp
+    )
+  }
+}
+
+renderToDom();
+
+export { renderToDom }
