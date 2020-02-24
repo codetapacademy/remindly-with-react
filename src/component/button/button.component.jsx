58d8d64584dea7aff4c94b1423eef5f11c7a7460
biancalina <bianca.alyna30@gmail.com>
2020-02-24 18:45:21 +0000

    started to test if the button get render

---
+++ src/component/button/button.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+  <button
+    onClick={onClick}
+    data-testid="nice-button">
+    helloagain
+  </button>
+  )
+}
+
+
+export { Button }
