92eca567fa4fb2c9a7f038f5f5090df752c59f4c
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-02-27 15:55:05 +0000

    make the basic setUp and install styled component, testing library and test the index.

---
+++ src/component/app/app.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReamindlyApp = () => {
+  return <div>Hello</div>;
+};
+
+export { ReamindlyApp };
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,14 @@
-console.log("More React :) ");
+import React from "react";
+import { render } from "react-dom";
+import { ReamindlyApp } from "./component/app";
+
+const renderToDom = () => {
+  const remindlyApp = document.querySelector("#remindly-app");
+  if (remindlyApp !== null) {
+    render(<ReamindlyApp />, remindlyApp);
+  }
+};
+
+renderToDom();
+
+export { renderToDom };
