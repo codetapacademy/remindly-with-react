fd6386fd37c460daf8833da9d7a4e5161afc0ccf
Bogdan Negura <negura.bogdan.90@gmail.com>
2020-02-27 21:15:16 +0000

    feat: [BogdanNegura] - trying to generate the days in calendar using moment

--- src/component/calendar/calendar.component.jsx
+++ src/component/calendar/calendar.component.jsx
@@ -8,8 +8,21 @@ const Calendar = () => {
       .weekday(n)
       .format("dddd")
   );
+
+  const firstDayInMonth = moment()
+    .date(0)
+    .hour(0)
+    .minute(0)
+    .second(0);
+  const firstDayInCalendar = firstDayInMonth.subtract(
+    firstDayInMonth.weekday(),
+    "d"
+  );
+  // .format();
+  console.log(firstDayInCalendar);
+
   const renderCalendarHeader = () => {
-    return weekDays.map(day => <div>{day}</div>);
+    return weekDays.map(day => <div key={day}>{day}</div>);
   };
   console.log(weekDays);
   return <StyledCalendar>{renderCalendarHeader()}</StyledCalendar>;
