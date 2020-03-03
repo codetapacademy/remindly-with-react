d9f57b00f3fd0533a543a2bf28b28417f93ef13d
biancalina <bianca.alyna30@gmail.com>
2020-03-03 16:42:28 +0000

    generate dates for the calendar

--- src/component/calendar/calendar.component.jsx
+++ src/component/calendar/calendar.component.jsx
@@ -6,16 +6,56 @@ const Calendar = () => {
   const weekdays = Array
     .from({length: 7}, (_, key) => key + 1)
     .map(n => moment().weekday(n).format('dddd'))
-    console.log(weekdays)
 
-    const renderCalendarHeader = () => {
-      return weekdays.map(day => (
-      <div>{day}</div>
-      ))
-    }
+  // const firstDayInMonth = moment()
+  //   .locale('en-gb')
+  //   .date(1)
+  //   .hour(0)
+  //   .minute(0)
+  //   .second(0)
+  const firstDayInCalendar = moment()
+    .locale('en-gb')
+    .date(1)
+    .hour(3)
+    .minute(0)
+    .second(0)
+    .subtract (
+      moment()
+        .locale('en-gb')
+        .date(1)
+        .hour(3)
+        .minute(0)
+        .second(0).weekday(),
+      'd'
+)
+
+    // .format()
+  const firstUnixTime = firstDayInCalendar.unix()
+  const dayList = Array
+    .from({length: 35}, (_,key) => key)
+    .map(n => ({
+      time: firstUnixTime*1000 + n*60*60*24*1000
+    }))
+    .map(({time}) => ({
+      time,
+      date: moment(time).format('MMMM Do YYYY')
+    }))
+  console.log(dayList)
+
+  const renderCalendarHeader = () => {
+    return weekdays.map(day => (
+    <div key={day}>{day}</div>
+    ))
+  }
+  const renderCalendarBody = () => {
+    return dayList.map(({date}) => (
+    <div key={date}>{date}</div>
+    ))
+  }
   return (
     <StyledCalendar>
       {renderCalendarHeader()}
+      {renderCalendarBody()}
     </StyledCalendar>
   )
 }
