00d98698b4e1870f5899a86514f120207f1d0d36
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-03-19 14:07:35 +0000

    having fun with moment library generating the days into the calendar. This closes #60

--- src/component/calendar/calendar.component.jsx
+++ src/component/calendar/calendar.component.jsx
@@ -6,7 +6,20 @@ const Calendar = () => {
   const weekDays = Array.from({length: 7}, (_, key) => key + 1).map(n => moment().weekday(n).format('dddd'))
   console.log(weekDays)
 
-const displayCalendarHeader = () => weekDays.map( day => (<div key={day}>{day}</div>))
+  const firstDayInMonth = moment()
+    .date(0)
+    .hour(0)
+    .minute(0)
+    .second(0)
+  const firstDayInCalendar = firstDayInMonth
+    .subtract(
+      firstDayInMonth.day(),
+      'd'
+      )
+
+  console.log(firstDayInCalendar)
+
+  const displayCalendarHeader = () => weekDays.map( day => (<div key={day}>{day}</div>))
 
   return (
     <StyledCalendar>
