3e50ec4666a6945a9a572caf360c47bee5e47762
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-03-19 11:40:42 +0000

    no more warning from react, added an unique key.

--- src/component/calendar/calendar.component.jsx
+++ src/component/calendar/calendar.component.jsx
@@ -6,7 +6,7 @@ const Calendar = () => {
   const weekDays = Array.from({length: 7}, (_, key) => key + 1).map(n => moment().weekday(n).format('dddd'))
   console.log(weekDays)
 
-const displayCalendarHeader = () => weekDays.map( day => (<div>{day}</div>))
+const displayCalendarHeader = () => weekDays.map( day => (<div key={day}>{day}</div>))
 
   return (
     <StyledCalendar>
