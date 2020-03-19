ee5af323f1fbd019f24050ca1c2128f4e8c2843d
Robert Alexandru <romaniarobertverde@yahoo.com>
2020-03-19 15:14:46 +0000

    generate 35 dates for our body calendar and display them into the browser. This closes #61

--- src/component/calendar/calendar.component.jsx
+++ src/component/calendar/calendar.component.jsx
@@ -6,24 +6,44 @@ const Calendar = () => {
   const weekDays = Array.from({length: 7}, (_, key) => key + 1).map(n => moment().weekday(n).format('dddd'))
   console.log(weekDays)
 
-  const firstDayInMonth = moment()
-    .date(0)
+  const firstDayInCalendar = moment()
+    .locale('en-gb')
+    .date(1)
     .hour(0)
     .minute(0)
     .second(0)
-  const firstDayInCalendar = firstDayInMonth
     .subtract(
-      firstDayInMonth.day(),
+      moment()
+        .locale('en-gb')
+        .date(1)
+        .hour(0)
+        .minute(0)
+        .second(0)
+        .weekday(),
       'd'
-      )
+    )
 
-  console.log(firstDayInCalendar)
+    const firstUnixTime = firstDayInCalendar.unix()
+    const dayList = Array
+      .from({length: 35}, (_, key) => key)
+      .map (n => ({
+        time: firstUnixTime*1000 + n*60*60*24*1000
+      }))
+      .map(({time}) => ({
+        time,
+        date: moment(time).format('MMMM Do YYYY')
+      }))
+
+  console.log(dayList)
 
   const displayCalendarHeader = () => weekDays.map( day => (<div key={day}>{day}</div>))
+  
+  const displayCalendarBody = () => dayList.map( ({date}) => (<div key={date}>{date}</div>))
 
   return (
     <StyledCalendar>
       {displayCalendarHeader()}
+      {displayCalendarBody()}
     </StyledCalendar>
   )
 }
